2b6bb8c849f366d1bdea8b50a5a14d0d7a8b8abe
Madhu Sunke <MadhuSunke417@users.noreply.github.com>
2022-05-26 06:33:12 -0400

    Add files via upload

---
+++ Download-Docker.ps1
@@ -0,0 +1,8 @@
+﻿cls
+$build = Invoke-RestMethod -UseBasicParsing https://desktop.docker.com/win/main/amd64/appcast.xml | select -ExpandProperty title
+$build = (($build.Split(" ")[-1]).trimstart("(")).trimend(")")
+$url   = "https://desktop.docker.com/win/main/amd64/BUILD/Docker%20Desktop%20Installer.exe"
+$url   = $url -replace "BUILD",$build
+$DockerInstaller = Join-Path $Env:Temp 'Docker Desktop Installer.exe'
+if(Test-Path $DockerInstaller){Remove-Item -Path $DockerInstaller -Force -ErrorAction SilentlyContinue}
+Invoke-WebRequest -Uri $url -OutFile $DockerInstaller -UseBasicParsing
