c0ec3cf8a2e872037bc7c41dad2cd5994cc18716
Madhu Sunke <MadhuSunke417@users.noreply.github.com>
2022-05-31 13:16:47 -0400

    Add files via upload

---
+++ Enable-NetFx3.ps1
@@ -0,0 +1,19 @@
+﻿$object = Get-WindowsOptionalFeature -Online -FeatureName "NetFx3"
+
+if($object.State -ne 'Enabled'){
+    try{
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWuServer" -Value 0
+    Restart-Service wuauserv
+    Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3" -ErrorAction Stop
+     }
+     catch{
+         Write-Host "$($_.exception.message)"
+         exit 1
+        }
+ }else{
+     Write-Output "Current State is : $($object.State)" -Verbose
+     exit 0
+ }
+
+  Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWuServer" -Value 1
+  Restart-Service wuauserv
